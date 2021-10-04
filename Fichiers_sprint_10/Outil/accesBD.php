db4a45ee2daacebe0baa2f1383bbd3790db410ca
antomuz <85632778+antomuz@users.noreply.github.com>
2021-10-04 16:39:39 +0200

    correction d'un bindvalue
    dans la classe listeDesNouvellesPourUnType

--- Fichiers_sprint_10/Outil/accesBD.php
+++ Fichiers_sprint_10/Outil/accesBD.php
@@ -139,7 +139,8 @@ class accesBD
 	
 	public function listeDesNouvellesPourUnType($idTypeNouvelleChoisi)
 	{
-		$requete = $this->conn->prepare('SELECT idNouvelle, dateParutionNouvelle, descriptionNouvelle FROM nouvelle where idTypeNouvelle =  '.$idTypeNouvelleChoisi.' ;');
+		$requete = $this->conn->prepare('SELECT idNouvelle, dateParutionNouvelle, descriptionNouvelle FROM nouvelle where idTypeNouvelle = ? ;');
+		$requet->bindValue(1,$idTypeNouvelleChoisi);
 		if($requete->execute()) 
 		{
 			$retour='';
