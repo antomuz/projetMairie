ef08980a31964c936c9ca8171d05f78ee5983894
Ardesisme <31305381+Ardesisme@users.noreply.github.com>
2021-10-04 10:34:41 +0200

    maj mise en forme connexion.php
    BlackCappedChickadee.tar.gz

--- Fichiers_sprint_10/Vues/ihm/connexion.php
+++ Fichiers_sprint_10/Vues/ihm/connexion.php
@@ -5,54 +5,54 @@
 
 <!-- La modal -->
 <div class="modal fade" id="connexion" tabindex="-1" role="dialog" aria-labelledby="maConnexion" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="maConnexion">Saisir vos identifiants</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-		<div class="container">
-			<div class="row">
+  	<div class="modal-dialog" role="document">
+    	<div class="modal-content">
+      		<div class="modal-header" style="text-align:center">
+        		<h5 class="modal-title" id="maConnexion">Saisir vos identifiants</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+      		</div>
+			<div class="modal-body">
+				<div class="container">
 					<form action=index.php?vue=Connexion&action=Verification method=POST align=center>
-						<table class="table table-sm">
-							<tbody>
-								<tr>
-									<td >
-											<input type="radio" name="role" value="1" id="admin">
-											<label for="admin">Admin</label> <br/>
-									</td>
-									<td>
-											<input type="radio" name="role" value="2" id="adherent">
-											<label for="adherent">Adherent</label> <br/>
-									</td>
-									<td>
-											<input type="radio" name="role" value="3" id="entraineur">
-											<label for="entraineur">Entraineur</label> <br/>
-									</td>
-								</tr>
-								<tr>
-									<td>
-											<input type=text name=login value="Login"></input>
-									</td>
-									<td>
-											<input type=password name=pwd value="Pwd"></input>
-									</td>
-									<td>
-									<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-									<button type="submit" class="btn btn-primary">Valider</button>
+						<div class="row">
+							<div class="col-sm">
+								<input type="radio" name="role" value="1" id="admin">
+								<label for="admin">Admin</label>
+							</div>
+							
+							<div class="col-sm">
+								<input type="radio" name="role" value="2" id="adherent">
+								<label for="adherent">Adherent</label>
+							</div>
+							
+							<div class="col-sm">
+								<input type="radio" name="role" value="3" id="entraineur">
+								<label for="entraineur">Entraineur</label>
+							</div>
+						</div>
+						<div class="row" style="padding-top:1em">
+							<div class="col-sm">
+								<input type=text name=login value="Login"></input>
+							</div>
+						</div>
+
+						<div class="row" style="padding-top:1em">
+							<div class="col-sm">
+								<input type=password name=pwd value="Pwd"></input>
+							</div>
+						</div>
+
+						<div class="row" style="padding-top:1em">
+							<div class="col-sm">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+								<button type="submit" class="btn btn-primary">Valider</button>
+							</div>
+						</div>	
 					</form>
-									</td>
-								</tr>
-							</tbody>
-						</table>
-						
+				</div>
 			</div>
 		</div>
-	  </div>
-      
-    </div>
-  </div>
+	</div>
 </div>
