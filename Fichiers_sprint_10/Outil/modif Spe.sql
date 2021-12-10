28dfeb9b170e5a18e01f9b4770ab559c86869cf3
antomuz <85632778+antomuz@users.noreply.github.com>
2021-12-10 12:30:53 +0100

    mise a jour des modification en rapport a l'ajout de specialité

--- Fichiers_sprint_10/Outil/modif Spe.sql
+++ Fichiers_sprint_10/Outil/modif Spe.sql
@@ -1,23 +1,59 @@
-create table specialite
-(
-	idSpe int not null,
-	NOM char(32) null,
-	constraint PK_specialite primary key (idSpe)
-);
+DROP TABLE IF EXISTS `specialite`;
+CREATE TABLE IF NOT EXISTS `specialite` (
+  `idSpe` int(11) NOT NULL AUTO_INCREMENT,
+  `nomSpe` char(32) NOT NULL,
+  PRIMARY KEY (`idSpe`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-alter table equipe
-add column idSpe int;
+INSERT INTO `specialite` (`idSpe`, `nomSpe`) VALUES
+(1, 'natation'),
+(2, 'foot'),
+(3, 'judo'),
+(4, 'equitation'),
+(5, 'volley'),
+(6, 'athletisme'),
+(7, 'moto cross');
 
-alter table equipe
-add foreign key(idSpe)
-references specialite(idSpe);
+DROP TABLE IF EXISTS `equipe`;
+CREATE TABLE IF NOT EXISTS `equipe` (
+  `idEquipe` int(11) NOT NULL AUTO_INCREMENT,
+  `nomEquipe` char(32) NOT NULL,
+  `nbrPlaceEquipe` int(11) NOT NULL,
+  `ageMinEquipe` int(11) NOT NULL,
+  `ageMaxEquipe` int(11) NOT NULL,
+  `sexeEquipe` char(1) NOT NULL,
+  `idEntraineur` int(11) NOT NULL,
+  `idSpe` int(11) NOT NULL,
+  PRIMARY KEY (`idEquipe`),
+  KEY `fk_equipe_entraineur` (`idEntraineur`),
+  KEY `fk_equipe_specialite` (`idSpe`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-create table speEntraineur
-(
-	idSpe int not null,
-	idEntraineur int not null,
-	constraint PK_speEntraineur primary key (idSpe, idEntraineur),
-	constraint FK_speEntraineur_entraineur foreign key (idSpe) references entraineur (idSpe),
-	constraint FK_speEntraineur_specialite foreign key (idEntraineur) references specialite (idEntraineur)
-);
+INSERT INTO `equipe` (`idEquipe`, `nomEquipe`, `nbrPlaceEquipe`, `ageMinEquipe`, `ageMaxEquipe`, `sexeEquipe`, `idEntraineur`, `idSpe`) VALUES
+(1, 'natation', 10, 5, 8, 'F', 3, 1),
+(2, 'foot', 20, 10, 12, 'F', 2, 2),
+(3, 'judo', 10, 5, 8, 'F', 1, 3),
+(4, 'equitation', 10, 5, 8, 'F', 1, 4),
+(5, 'volley', 10, 5, 8, 'F', 1, 5),
+(6, 'athletisme', 10, 5, 8, 'F', 1, 6),
+(7, 'moto cross', 10, 5, 8, 'F', 1, 7);
+
+DROP TABLE IF EXISTS `spe_entraineur`;
+CREATE TABLE IF NOT EXISTS `Spe_Entraineur` (
+  `idSpe` int(11) NOT NULL,
+  `idEntraineur` int(11) NOT NULL,
+  PRIMARY KEY (`idSpe`,`idEntraineur`),
+  KEY `fk_specialite_spe_entraineur` (`idSpe`),
+  KEY `fk_entraineur_spe_entraineur` (`idEntraineur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+ALTER TABLE `equipe`
+  ADD CONSTRAINT `fk_equipe_entraineur` FOREIGN KEY (`idEntraineur`) REFERENCES `entraineur` (`idEntraineur`),
+  ADD CONSTRAINT `fk_equipe_specialite` FOREIGN KEY (`idSpe`) REFERENCES `specialite` (`idSpe`);
+
+ALTER TABLE `spe_entraineur`
+  ADD CONSTRAINT `fk_specialite_spe_entraineur` FOREIGN KEY (`idSpe`) REFERENCES `specialite` (`idSpe`),
+  ADD CONSTRAINT `fk_entraineur_spe_entraineur` FOREIGN KEY (`idEntraineur`) REFERENCES `entraineur` (`idEntraineur`);
 
