68daaccad9bbf9600207f44f7c2d7fb7facc612d
antomuz <85632778+antomuz@users.noreply.github.com>
2021-12-10 15:00:21 +0100

    ajout d'une table faisant lien entre les adhérents et les équipes cardinalité N,N

--- Fichiers_sprint_10/Outil/modif Spe.sql
+++ Fichiers_sprint_10/Outil/modif Spe.sql
@@ -14,6 +14,8 @@ INSERT INTO `specialite` (`idSpe`, `nomSpe`) VALUES
 (6, 'athletisme'),
 (7, 'moto cross');
 
+
+
 DROP TABLE IF EXISTS `equipe`;
 CREATE TABLE IF NOT EXISTS `equipe` (
   `idEquipe` int(11) NOT NULL AUTO_INCREMENT,
@@ -38,6 +40,30 @@ INSERT INTO `equipe` (`idEquipe`, `nomEquipe`, `nbrPlaceEquipe`, `ageMinEquipe`,
 (6, 'athletisme', 10, 5, 8, 'F', 1, 6),
 (7, 'moto cross', 10, 5, 8, 'F', 1, 7);
 
+
+
+DROP TABLE IF EXISTS `adherent`;
+CREATE TABLE IF NOT EXISTS `adherent` (
+  `idAdherent` int(11) NOT NULL AUTO_INCREMENT ,
+  `nomAdherent` char(32) NOT NULL,
+  `prenomAdherent` char(32) NOT NULL,
+  `ageAdherent` int(11) NOT NULL,
+  `sexeAdherent` char(1) NOT NULL,
+  `loginAdherent` char(20) NOT NULL,
+  `pwdAdherent` varchar(200) NOT NULL,
+  PRIMARY KEY (`idAdherent`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `adherent` (`idAdherent`, `nomAdherent`, `prenomAdherent`, `ageAdherent`, `sexeAdherent`, `loginAdherent`, `pwdAdherent`) VALUES
+(1, 'Dupont', 'Pierre', 8, 'F', 'pDupont',md5('pDupont')),
+(2, 'Dubois', 'Vincent', 10, 'M','vDubois',md5('vDubois')),
+(3, 'Durant', 'Jacques', 6, 'M','jDurant',md5('jDurant')),
+(4, 'Fleur', 'Sophie', 7, 'F','sFleur',md5('sFleur')),
+(5, 'Seimei' , 'Abe', 9, 'M', 'aSeimei', md5('aSeimei')),
+(6, 'Kirigaya', 'Kazuto', 11, 'M', 'kKirigaya', md5('kKirigaya'));
+
+
+
 DROP TABLE IF EXISTS `spe_entraineur`;
 CREATE TABLE IF NOT EXISTS `Spe_Entraineur` (
   `idSpe` int(11) NOT NULL,
@@ -45,6 +71,15 @@ CREATE TABLE IF NOT EXISTS `Spe_Entraineur` (
   PRIMARY KEY (`idSpe`,`idEntraineur`),
   KEY `fk_specialite_spe_entraineur` (`idSpe`),
   KEY `fk_entraineur_spe_entraineur` (`idEntraineur`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `equipe_adherent`;
+CREATE TABLE IF NOT EXISTS `equipe_adherent` (
+  `idAdherent` int(11) NOT NULL,
+  `idEquipe` int(11) NOT NULL,
+  PRIMARY KEY (`idAdherent`,`idEquipe`),
+  KEY `fk_adherent_equipe_adherent` (`idAdherent`),
+  KEY `fk_equipe_equipe_adherent` (`idEquipe`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -56,4 +91,8 @@ ALTER TABLE `equipe`
 ALTER TABLE `spe_entraineur`
   ADD CONSTRAINT `fk_specialite_spe_entraineur` FOREIGN KEY (`idSpe`) REFERENCES `specialite` (`idSpe`),
   ADD CONSTRAINT `fk_entraineur_spe_entraineur` FOREIGN KEY (`idEntraineur`) REFERENCES `entraineur` (`idEntraineur`);
+
+ALTER TABLE `equipe_adherent`
+  ADD CONSTRAINT `fk_adherent_equipe_adherent` FOREIGN KEY (`idAdherent` ) REFERENCES `adherent` (`idAdherent` ),
+  ADD CONSTRAINT `fk_equipe_equipe_adherent` FOREIGN KEY (`idEquipe`) REFERENCES `equipe` (`idEquipe`);
 
