7ecc07ee3da436ae5c8c16c0a351c5b4c31f8bee
Ardesisme <31305381+Ardesisme@users.noreply.github.com>
2022-04-08 09:19:58 +0200

    Add files via upload

--- Fichiers_sprint_10/Traitement/classeConteneur/conteneurEntraineur.php
+++ Fichiers_sprint_10/Traitement/classeConteneur/conteneurEntraineur.php
@@ -2,107 +2,86 @@
 
 
 class conteneurEntraineur
-	{
+{
 	//attribut de type arrayObjet, mais on est en php donc on ne met pas les types
 	private $lesEntraineurs;
-	
+
 	//le constructeur créer un tableau vide
 	public function __construct()
-		{
+	{
 		$this->lesEntraineurs = new arrayObject();
-		}
-	
+	}
+
 	//les méthodes habituellement indispensables
 	public function ajouterUnEntraineur($unIdEntraineur, $unNomEntraineur, $unLoginEntraineur, $unPwdEntraineur)
 	{
 		$unEntraineur = new metierEntraineur($unIdEntraineur, $unNomEntraineur, $unLoginEntraineur, $unPwdEntraineur);
 		$this->lesEntraineurs->append($unEntraineur);
-			
 	}
-	
+
 	public function nbEntraineur()
-		{
+	{
 		return $this->lesEntraineurs->count();
-		}	
-		
+	}
+
 	public function listeDesEntraineurs()
-		{
+	{
 		$liste = '';
-		foreach ($this->lesEntraineurs as $unEntraineur)
-			{	$liste = $liste.$unEntraineur->afficheEntraineur().'|';
-			}
-		
-		return $liste;
+		foreach ($this->lesEntraineurs as $unEntraineur) {
+			$liste = $liste . $unEntraineur->afficheEntraineur() . '|';
 		}
 
-	
-		
-	public function lesEntraineursAuFormatHTML()
-		{
-		$liste = "<SELECT name = 'idEntraineur'>";
-		foreach ($this->lesEntraineurs as $unEntraineur)
-			{
-			$liste = $liste."<OPTION value='".$unEntraineur->getIdEntraineur()."'>".$unEntraineur->getNomEntraineur()."</OPTION>";
-			}
-		$liste = $liste."</SELECT>";
-		return $liste;
-		}	
-		
-	public function lesEntraineursAuFormatHTMLPourModif($Entraineur_cible) 
-	{
-		$liste = "<SELECT name = 'idTitulaire'>";
-		foreach ($this->lesEntraineurs as $unEntraineur)
-			{
-				if($unEntraineur->getIdEntraineur() == $Entraineur_cible->getIdEntraineur()) 
-				{
-					$liste = $liste."<OPTION value='".$unEntraineur->getIdEntraineur()."' selected>".$unEntraineur->getNomEntraineur()."</OPTION>";
-				}
-				else 
-				{
-					$liste = $liste."<OPTION value='".$unEntraineur->getIdEntraineur()."'>".$unEntraineur->getNomEntraineur()."</OPTION>";
-				}
-			}
-		$liste = $liste."</SELECT>";
 		return $liste;
 	}
-	
-	public function donneObjetEntraineurDepuisNumero($unIdEntraineur)
-		{
-		$trouve=false;
-		$leBonEntraineur=null;
-		$iEntraineur = $this->lesEntraineurs->getIterator();
-		while ((!$trouve)&&($iEntraineur->valid()))
-			{
-			if ($iEntraineur->current()->getIdEntraineur()==$unIdEntraineur)
-				{
-				$trouve=true;
-				$leBonEntraineur = $iEntraineur->current();
-				}
-			else
-				$iEntraineur->next();
-			}
-		return $leBonEntraineur;
+
+
+
+	public function lesEntraineursAuFormatHTML()
+	{
+		$liste = "<SELECT name = 'idEntraineur'>";
+		foreach ($this->lesEntraineurs as $unEntraineur) {
+			$liste = $liste . "<OPTION value='" . $unEntraineur->getIdEntraineur() . "'>" . $unEntraineur->getNomEntraineur() . "</OPTION>";
 		}
-	
+		$liste = $liste . "</SELECT>";
+		return $liste;
+	}
+
+	public function donneObjetEntraineurDepuisNumero($unIdEntraineur)
+	{
+		$trouve = false;
+		$leBonEntraineur = null;
+		$iEntraineur = $this->lesEntraineurs->getIterator();
+		while ((!$trouve) && ($iEntraineur->valid())) {
+			if ($iEntraineur->current()->getIdEntraineur() == $unIdEntraineur) {
+				$trouve = true;
+				$leBonEntraineur = $iEntraineur->current();
+			} else
+				$iEntraineur->next();
+		}
+		return $leBonEntraineur;
+	}
+
 	public function donneObjetEntraineurDepuisLogin($loginEntraineur)
 	{
-	$trouve=false;
-	$leBonEntraineur=null;
-	$iEntraineur = $this->lesEntraineurs->getIterator();
-	while ((!$trouve)&&($iEntraineur->valid()))
-		{
-		if ($iEntraineur->current()->getLoginEntraineur()==$loginEntraineur)
-			{
-			$trouve=true;
-			$leBonEntraineur = $iEntraineur->current();
-			}
-		else
-			$iEntraineur->next();
+		$trouve = false;
+		$leBonEntraineur = null;
+		$iEntraineur = $this->lesEntraineurs->getIterator();
+		while ((!$trouve) && ($iEntraineur->valid())) {
+			if ($iEntraineur->current()->getLoginEntraineur() == $loginEntraineur) {
+				$trouve = true;
+				$leBonEntraineur = $iEntraineur->current();
+			} else
+				$iEntraineur->next();
 		}
-	return $leBonEntraineur;
+		return $leBonEntraineur;
 	}
 
-
-			
+	public function modifierMDP($entraineur, $MDP)
+	{
+		foreach ($this->lesEntraineurs as $unEntraineur) {
+			if ($unEntraineur->getIdEntraineur() == $entraineur->getIdEntraineur()) {
+				$unEntraineur->setPWDEntraineur($MDP);
+			}
+		}
 	}
-?> 
+}
--- Fichiers_sprint_10/Vues/ihm/vueCentraleAdherent.php
+++ Fichiers_sprint_10/Vues/ihm/vueCentraleAdherent.php
@@ -1,17 +1,16 @@
 <?php
-	
-	class vueCentraleAdherent
+
+class vueCentraleAdherent
+{
+	public function __construct()
 	{
-		public function __construct()
-		{
-			
-		}
-		
-		public function visualiserAdherent($message)
-		{		
-			$listeAdherent=explode("|",$message);
-			
-			echo '<br><table class="table table-striped table-bordered table-sm ">
+	}
+
+	public function visualiserAdherent($message)
+	{
+		$listeAdherent = explode("|", $message);
+
+		echo '<br><table class="table table-striped table-bordered table-sm ">
 		 			<thead>
 		 				<tr>
 		 					<th scope="col">Nom</th>
@@ -21,27 +20,25 @@
 		 				</tr>
 		 			</thead>
 		 			<tbody>';
-		 	$nbE=0;
-		 	while ($nbE<sizeof($listeAdherent))
-		 	{	
-		 		$i=0;
-		 		while (($i<4) && ($nbE<sizeof($listeAdherent)))
-		 		{
-		 			echo '<td scope>';
-		 			echo trim($listeAdherent[$nbE]);
-		 			$i++;
-		 			$nbE++;
-		 			echo '</td>';
-		 		}
-		 		echo '</tr>';
-		 	}
-		 	echo '</tbody>';
-		 	echo '</table>';
-			
+		$nbE = 0;
+		while ($nbE < sizeof($listeAdherent)) {
+			$i = 0;
+			while (($i < 4) && ($nbE < sizeof($listeAdherent))) {
+				echo '<td scope>';
+				echo trim($listeAdherent[$nbE]);
+				$i++;
+				$nbE++;
+				echo '</td>';
+			}
+			echo '</tr>';
 		}
-		
-		public function saisirAdherent($liste){
-			echo('
+		echo '</tbody>';
+		echo '</table>';
+	}
+
+	public function saisirAdherent($liste)
+	{
+		echo ('
 			<form action=index.php?vue=Adherent&action=enregistrer method=POST align=center>
 			<legend>Caractéristiques de l\'adhérent :</legend>
 			<table class="table table-bordered table-sm table-striped">
@@ -65,8 +62,8 @@
 
 				<tr>
 					<td>');
-			echo($liste);
-			echo('
+		echo ($liste);
+		echo ('
 					</td>
 				</tr>
 						
@@ -81,47 +78,48 @@
 					<td><button type="submit" class="btn btn-primary">Valider</button></td>
 				</tr>
 			</table>
-			</form>');			
-		}
+			</form>');
+	}
 
 
-		public function voyagerAdherent()
-		{		
-			echo '<iframe width=100% height=150% src="https:www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.319959224129!2d1.3158100143582203!3d43.683111158516006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aeaee6224c0f13%3A0x9f57b169fe3a7161!2sMairie!5e0!3m2!1sfr!2sfr!4v1626195896682!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
-				
-		}
-		
-	public function voirProfil($adherent){
+	public function voyagerAdherent()
+	{
+		echo '<iframe width=100% height=150% src="https:www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.319959224129!2d1.3158100143582203!3d43.683111158516006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aeaee6224c0f13%3A0x9f57b169fe3a7161!2sMairie!5e0!3m2!1sfr!2sfr!4v1626195896682!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
+	}
 
-			echo "
+	public function voirProfil($adherent)
+	{
+
+		echo "
 			PLACEHOLDER POUR LA PHOTO
 			<table>
 			<tr>
 			<td>Nom</td>
-			<td>".$adherent->getNomAdherent()."</td>
+			<td>" . $adherent->getNomAdherent() . "</td>
 			</tr>
 			<tr>
 			<td>Prénom</td>
-			<td>".$adherent->getPrenomAdherent()."</td>
+			<td>" . $adherent->getPrenomAdherent() . "</td>
 			</tr>
 			<tr>
 			<td>Âge</td>
-			<td>".$adherent->getAgeAdherent()."</td>
+			<td>" . $adherent->getAgeAdherent() . "</td>
 			</tr>
 			<tr>
 			<td>Sexe</td>
-			<td>".$adherent->getSexeAdherent()."</td>
+			<td>" . $adherent->getSexeAdherent() . "</td>
 			</tr>
 			<tr>
 			<td>Login</td>
-			<td>".$adherent->getLoginAdherent()."</td>
+			<td>" . $adherent->getLoginAdherent() . "</td>
 			</tr>
 			</table>";
-		}
+	}
 
-	public function modifierAdherent($message){
+	public function modifierAdherent($message)
+	{
 		echo '<form action=index.php?vue=Adherent&action=choixFaitPourModif method = GET>';
-		echo $message; 
+		echo $message;
 		echo ' <input type=hidden name=vue value=Adherent></input>
 			   <input type=hidden name=action value=choixFaitPourModif></input>
 			   <button type="submit" class="btn btn-primary">Valider</button>
@@ -132,62 +130,59 @@
 	public function choixFaitPourModifAdherent($nom, $prenom, $age, $sexe, $login, $choix)
 	{
 		echo '<form action=index.php?vue=Adherent&action=EnregModif method = POST>
-						<input type=text name=nomAdherent value='.$nom.'></input>
-						<input type=integer name=prenomAdherent value='.$prenom.'></input>
-						<input type=integer name=ageAdherent value='.$age.'></input>
-						<input type=integer name=sexeAdherent value='.$sexe.'></input>
-						<input type=text name=loginAdherent value='.$login.'></input>
-						<input type=hidden name=idAdherent value='.$choix.'></input>
+						<input type=text name=nomAdherent value=' . $nom . '></input>
+						<input type=integer name=prenomAdherent value=' . $prenom . '></input>
+						<input type=integer name=ageAdherent value=' . $age . '></input>
+						<input type=integer name=sexeAdherent value=' . $sexe . '></input>
+						<input type=text name=loginAdherent value=' . $login . '></input>
+						<input type=hidden name=idAdherent value=' . $choix . '></input>
 						<input type="submit" class="btn btn-primary" name="Valider" value="Valider"></input>
 						<br>
 						<input type="submit" name="resMDP" value="resMDP"class="btn btn-primary
 			 </form>';
 	}
 
-	public function modifierProfil($adherent){
+	public function modifierProfil($adherent)
+	{
 		//incorpore ça idiot <form action=index.php?vue=Entraineur&action=SaisirEntraineur method=POST align=center>
-		echo $adherent->getSexeAdherent();
+
 		echo "PLACEHOLDER POUR LA PHOTO
 			<form action=index.php?vue=Adherent&action=EnregistrerProfil method=POST align=center>
 			<table>
 			<tr>
 			<td>Nom</td>
-			<td><input type='text' name='nomAdherent' value=".$adherent->getNomAdherent()." required='true'>
+			<td><input type='text' name='nomAdherent' value=" . $adherent->getNomAdherent() . " required='true'>
 			</tr>
 			<tr>
 			<td>Prénom</td>
-			<td><input type='text' name='prenomAdherent' value=".$adherent->getPrenomAdherent()." required='true'>
+			<td><input type='text' name='prenomAdherent' value=" . $adherent->getPrenomAdherent() . " required='true'>
 			</tr>
 			<tr>
 			<td>Âge</td>
-			<td><input type='text' name='ageAdherent' value=".$adherent->getAgeAdherent()." required='true'>
+			<td><input type='text' name='ageAdherent' value=" . $adherent->getAgeAdherent() . " required='true'>
 			</tr>
 			<tr>
 			<td>Sexe</td>
-			<td><select name='sexeAdherent' required='true'>";
-			if ($adherent->getSexeAdherent() == "F") {
-				echo "<option value='F' selected>Femme</option>
-				<option value='H'>Homme</option>";
-			}
-			else {
-				echo "<option value='F'>Femme</option>
-				<option value='H' selected>Homme</option>";
-			}
-			echo "</select>
+			<td><select name='sexeAdherent' required='true'>
+			<option value=" . $adherent->getSexeAdherent() . ">
+			<option value='F'>Femme</option>
+			<option value='H'>Homme</option>
+			</select>
 			</td>
 			</tr>
 			<tr>
 			<td>Login</td>
-			<td><input type='text' name='loginAdherent' value=".$adherent->getLoginAdherent()." required='true'>
+			<td><input type='text' name='loginAdherent' value=" . $adherent->getLoginAdherent() . " required='true'>
 			</tr>
-			<input type='hidden' name='idAdherent' value=".$adherent->getIdAdherent().">
+			<input type='hidden' name='idAdherent' value=" . $adherent->getIdAdherent() . ">
 			</table>
 			<button type='submit' class='btn btn-primary'>Valider</button>
 			</form>";
 	}
 
-	public function changerMDP($erreur=false){
-		if($erreur){
+	public function changerMDP($erreur = false)
+	{
+		if ($erreur) {
 			echo "<p style='color:red'>Le mot de passe ne respecte pas les règles de construction de mot de passe !</p>";
 		}
 		echo "<form action=index.php?vue=Adherent&action=verifMDP method=POST align=center>
@@ -205,13 +200,11 @@
 		<li>Au moins 1 lettre majuscule</li>
 		<li>Au moins 1 chiffre</li>
 		<li>Au moins 1 caractère spécial</li>
-		</ul>"
-		;
+		</ul>";
 	}
 
-	public function confirmationChangement(){
+	public function confirmationChangement()
+	{
 		echo "Le changement a été effectué.";
-	}	
-	
+	}
 }
-?>
